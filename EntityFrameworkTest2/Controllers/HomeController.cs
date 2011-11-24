8705d5afbca5cd5bff62414c67fb7f8835536cc0
Michael Friis <friism@gmail.com>
2011-11-23 23:26:34 -0800

    get connectionstring differently

--- EntityFrameworkTest2/Controllers/HomeController.cs
+++ EntityFrameworkTest2/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 using System.Configuration;
+using EntityFrameworkTest2.Model;
 
 namespace EntityFrameworkTest2.Controllers
 {
@@ -7,7 +8,8 @@ namespace EntityFrameworkTest2.Controllers
 	{
 		public ActionResult Index()
 		{
-			var cs = ConfigurationManager.ConnectionStrings["dbee5034285b5e4e7885929fa50063900dEntities"].ConnectionString;
+			//var cs = ConfigurationManager.ConnectionStrings["dbee5034285b5e4e7885929fa50063900dEntities"].ConnectionString;
+			var cs = new dbee5034285b5e4e7885929fa50063900dEntities().Connection.ConnectionString;
 			return View((object)cs);
 		}
 	}
