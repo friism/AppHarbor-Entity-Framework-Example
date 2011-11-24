262ca63c14bed58be9c1252d59d5c32e6dafdcc4
Michael Friis <friism@gmail.com>
2011-11-23 22:48:54 -0800

    show cs

--- EntityFrameworkTest2/Controllers/FooController.cs
+++ EntityFrameworkTest2/Controllers/FooController.cs
@@ -7,7 +7,8 @@ namespace EntityFrameworkTest2.Controllers
 	{
 		public ActionResult Index()
 		{
-			var foos = new dbee5034285b5e4e7885929fa50063900dEntities().Foos;
+			var s = @"metadata=res://*/Model.Model1.csdl|res://*/Model.Model1.ssdl|res://*/Model.Model1.msl;provider=System.Data.SqlClient;provider connection string='Server=ee503428-5b5e-4e78-8592-9fa50063900d.sqlserver.sequelizer.com;Database=dbee5034285b5e4e7885929fa50063900d;User ID=llpfgusfzzkbogea;Password=pXDiwJpCUBBTXPSBeWVHKUtPyChhr3ufFrCuhqY2mhzUu6xok58TAJxBDWS3BhkZ;'";
+			var foos = new dbee5034285b5e4e7885929fa50063900dEntities(s).Foos;
 			return View(foos);
 		}
 
