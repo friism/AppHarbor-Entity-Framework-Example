1861370d9d8b7131959996e4a60b1ea016562abf
Michael Friis <friism@gmail.com>
2011-11-23 22:51:27 -0800

    show cs

---
+++ EntityFrameworkTest2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using System.Configuration;
+
+namespace EntityFrameworkTest2.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var cs = ConfigurationManager.ConnectionStrings["dbee5034285b5e4e7885929fa50063900dEntities"].ConnectionString;
+			return View((object)cs);
+		}
+	}
+}
