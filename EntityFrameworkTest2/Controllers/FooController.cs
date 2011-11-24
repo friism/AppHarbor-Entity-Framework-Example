d15a50eeaadfc0cc64963d70d1b4805f6a145bb4
Michael Friis <friism@gmail.com>
2011-11-23 22:29:04 -0800

    initial

---
+++ EntityFrameworkTest2/Controllers/FooController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using EntityFrameworkTest2.Model;
+
+namespace EntityFrameworkTest2.Controllers
+{
+	public class FooController : Controller
+	{
+		public ActionResult Index()
+		{
+			var foos = new dbee5034285b5e4e7885929fa50063900dEntities().Foos;
+			return View(foos);
+		}
+
+		public ActionResult New()
+		{
+			return View();
+		}
+
+		public ActionResult Create(Foo foo)
+		{
+			var blah = new dbee5034285b5e4e7885929fa50063900dEntities();
+			blah.Foos.AddObject(foo);
+			blah.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ EntityFrameworkTest2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFrameworkTest2.MvcApplication" Language="C#" %>
